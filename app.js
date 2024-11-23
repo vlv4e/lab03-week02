ea0ce46babd566dbe84c7b770593c6414b6f9dec
vlv4e <mhndbinbader@gmail.com>
2024-11-23 18:54:18 +0300

    ex7

--- app.js
+++ app.js
@@ -174,4 +174,87 @@ const game = {
     ],
   }
   
-  console.dir(pokemon, { maxArrayLength: null })
+//   console.dir(pokemon, { maxArrayLength: null })
+
+//   console.log(game)
+
+
+/*
+Exercise 3
+1. Add a new property to the `game` object. Let's call it "difficulty".
+2. Choose a value for "difficulty" that you think fits the game. Ex: "Easy", "Med" or "Hard". How would you assign it?
+
+
+Solve Exercise 3 here:
+*/
+
+
+game.difficulty = [1,2,3,4,5,6,7,8,9 ];
+console.log(game.difficulty)
+
+/*
+Exercise 4
+1. Select a starter Pokémon from the `pokemon` array. Remember, a starter Pokémon's `starter` property is true.
+2. Add this Pokémon to the `game.party` array. Which array method will you use to add them?
+
+
+Solve Exercise 4 here:
+*/
+
+
+console.log(pokemon.starter)
+for(let i = 0; i < pokemon.length; i++){
+if(pokemon[i].starter === true){
+game.party.push(pokemon[i])}
+}
+console.log(game.party)
+
+/*
+Exercise 5
+1. Choose three more Pokémon from the `pokemon` array and add them to your party.
+2. Consider different attributes like 'type' or 'HP' for your selection. Which array method will you use to add them?
+
+
+Solve Exercise 5 here:
+*/
+
+game.party.splice (4,0,  { number: 149, name: 'Dragonite', type: 'dragon', hp: 91, starter: false },
+{ number: 150, name: 'Mewtwo', type: 'psychic', hp: 106, starter: false },
+{ number: 151, name: 'Mew', type: 'psychic', hp: 100, starter: false }
+);
+console.log(game.party)
+
+/*
+Exercise 6
+1. Set the `completed` property to true for gyms with a difficulty below 3.
+2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
+
+
+Solve Exercise 6 here:
+*/
+
+console.log(game.gyms)
+game.gyms.forEach(gym => {
+    if (gym.difficulty < 3) {
+        gym.completed = true;
+    }
+});
+console.log(game.gyms)
+
+/*
+Exercise 7
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. When working with an array of objects, the splice() array method is ideal for replacing one element with another. 
+
+
+Solve Exercise 7 here:
+*/
+
